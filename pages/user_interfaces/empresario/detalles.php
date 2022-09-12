af940c6fa9919c3b0b12d025fe49643afd77513d
Ruben Espinoza Osses <ruben.espinoza.osses@gmail.com>
2022-09-12 16:33:12 -0300

    Cambios locales a descartar

--- pages/user_interfaces/empresario/detalles.php
+++ pages/user_interfaces/empresario/detalles.php
@@ -1,32 +1,58 @@
+<?php
+include '../../../sys/db_config.php';
+include '../../../db/entrepeneur/entrepeneur_handler.php';
+
+conexion::abrir_conexion();
+
+$rut_empresario = base64_decode($_GET['rut_empresario']);
+$empresario = class_operar_empresarios::buscar_empresarios_rut($rut_empresario, conexion::obtener_conexion());
+
+?>
+
+
 <!doctype html>
 <html lang="en">
-  <head>
-    <title><?php echo $_GET['nombre-cliente'] ?></title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS v5.2.0-beta1 -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"  integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+<head>
+  <title>Empresario</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-  </head>
-  <body>
-    <div class="card card-body" style="width: 50rem;">
-      <div class="row">
-        <h6 class="card-title"> Hola </h6>
+  <div style="display: block; text-align: center;">
+    <h4 text-aling="center">
+      Detalles <?php echo $empresario -> obtener_nombre(); ?>
+    </h4>
+  </div>
+
+  <div class="container-fluid" style="align-items: center; display: block;">
+    <section>
+
+      <div class="card" style="width: 18rem;">
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">Cras justo odio</li>
+          <li class="list-group-item">Dapibus ac facilisis in</li>
+          <li class="list-group-item">Vestibulum at eros</li>
+        </ul>
       </div>
-      <div class="row">
 
-      </div>
-      <div class="row">
-      </div>
-      <div class="row">
+    </section>
+  </div>
 
-      </div>
-    </div>
-    <!-- Bootstrap JavaScript Libraries -->
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+  <!-- Bootstrap CSS v5.2.0-beta1 -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
-  </body>
+</head>
+
+<body>
+
+
+
+
+  <!-- Bootstrap JavaScript Libraries -->
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
+</body>
+
 </html>
