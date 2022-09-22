faa054f66cca0cf10ea5794a2adeae5757a874ba
Ruben Espinoza Osses <ruben.espinoza.osses@gmail.com>
2022-09-22 11:06:32 -0300

    Se modifica el tiempo para la redireccion del login

--- middlewares/login.php
+++ middlewares/login.php
@@ -19,7 +19,7 @@ $login_valido = $validador -> login_valido();
 
 
 if (!$login_valido) {
-    header("refresh:3;url=../pages/login.php");
+    header("refresh:1;url=../pages/login.php");
     echo "<script type='text/javascript'>alert('Hubo un problema con su login1, intente nuevamente');</script>";
 
     
@@ -28,12 +28,11 @@ if (!$login_valido) {
 
     $usuario = class_operar_usuarios::buscar_usuarios_rut($rut, conexion::obtener_conexion());
     $user_id = $usuario -> obtener_id();
-    //echo "<script type='text/javascript'>alert('$user_id')</script>";
 
 
     control_sesion::iniciar_sesion($usuario->obtener_id(), $usuario->obtener_nombre(), $usuario->obtener_apellido_paterno(), $usuario->obtener_apellido_materno(), $usuario->obtener_rut(), $usuario->obtener_acceso(), tiempo_sesion);
 
-    header('Refresh:3;url=../pages/user_interfaces/seleccionar_empresario.php');
+    header('Refresh:1;url=../pages/user_interfaces/seleccionar_empresario.php');
 
     
 }
